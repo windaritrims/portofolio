6b13eb357ffc83b51d4f467f1b320a6c3da60c7a
7th <windaritrims@gmail.com>
2025-05-24 14:11:20 +0700

    menambah lanyard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+'use client';
+
+import Lanyard from "./components/Lanyard/Lanyard";
+// import Image from 'next/image'; // Uncomment if needed in future
 
 export default function Home() {
   return (
- <h1>windaritrims</h1>
+    <div className="min-h-screen overflow-x-hidden">
+      <div className="container border mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">winda</div>
+          <div className="col-span-6">
+            <Lanyard position={[0,0,20]} gravity={[0,-40,0]} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
