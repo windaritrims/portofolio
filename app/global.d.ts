88f02c5e6d77efecd94296ed0aff4c73a838bef4
7th <windaritrims@gmail.com>
2025-05-24 14:39:07 +0700

    6

--- app/global.d.ts
+++ app/global.d.ts
@@ -20,8 +20,8 @@ declare module 'meshline' {
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      meshLineGeometry: any;
-      meshLineMaterial: any;
+      meshLineGeometry: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      meshLineMaterial: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
     }
   }
 }
